56f1c7de6e738f9b6bf3315b4d0be2746fc90452
Ninad Chaudhari <65274398+Ninad-Chaudhari@users.noreply.github.com>
2021-05-16 02:20:04 +0530

    Update filterRecommendations.py

--- filterRecommendations.py
+++ filterRecommendations.py
@@ -8,7 +8,6 @@ def euclid(a, b):
 def getNeighbors(record, data, K):
     distances = []
     for i in range(data.shape[0]):
-      yield "<br/>"
       if data.iloc[i,0] != record.iloc[0,0]:
         dist = euclid(record.iloc[0, 1:].values, data.iloc[i, 1:].values)
         distances.append((data.iloc[i,0], dist))
